339604d018d9327421daad9c483b74fa211f7195
Ashwin Kallor <ashwinkallor@gmail.com>
2025-03-19 07:27:41 +0100

    Removed secret key for security reasons

--- proteomics_data_lake/aws_initial_setup.sh
+++ proteomics_data_lake/aws_initial_setup.sh
@@ -1,10 +1,9 @@
 # Install and configure AWS CLI
 pip install awscli
-
+#The secret key cannot be added here due to security reasons
 aws configure  # Enter your AWS credentials when prompted 
 
 #AWS Access ID: AKIAU6GDYU554DIVROMT
-#Secret Access ID: ELwi+GmPOwgBC1PBFwfVCXsyRQHjBrGBZ2OMBrfT
 #Default region name [ap-southeast-2]: ap-southeast-2
 #Default output format [json]: json
 
